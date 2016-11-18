963107e3cc23f6d1a6adb04b5f75c0ad3cd98a1c
Olivia Kasper <olivia.kasper@mmp.htwchur.ch>
2016-11-18 14:47:55 +0100

    ein bisschen was am Registration-Formular rumgebastelt. Also dass es pattern hat und dass mit der Fehlerausgabe funzt leider immer noch nicht. Werde.

--- php/login.php
+++ php/login.php
@@ -95,7 +95,7 @@ if(isset($_POST['login_submit'])){
 						<div class="form-group">
 						  <label class="control-label col-sm-2" for="email">E-Mail:</label>
 						  <div class="col-sm-10">
-							<input type="email" class="form-control" id="email" name="email" placeholder="max@muster.ch">
+							<input type="email" class="form-control" id="email" name="email" placeholder="max@muster.ch" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
 						  </div>
 						</div>
 						<div class="form-group">
