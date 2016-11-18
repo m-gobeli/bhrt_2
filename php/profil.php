bee181f11895305fd6aa33ade27a1b4e0ee50582
Maja Gobeli <maja.gobeli@mmp.htwchur.ch>
2016-11-18 08:41:23 +0100

    Link zu Profil korrigiert

--- php/profil.php
+++ php/profil.php
@@ -41,7 +41,7 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li>
-                        <a class="page-scroll" href="login.php">Profil</a>
+                        <a class="page-scroll" href="profil.php">Profil</a>
                     </li>
                     <li>
                         <a class="page-scroll" href="test.php">Test</a>
