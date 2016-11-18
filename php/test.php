a60aa6c5835642df75b193e49ef30284fe804283
Olivia Kasper <olivia.kasper@mmp.htwchur.ch>
2016-11-18 12:06:22 +0100

    pushing stuff as usual. didn’t do stuff as usual. schaut man den verlauf an könnte man meinen ich mache nix und haue schräg mal irgendwelche codes rein

--- php/test.php
+++ php/test.php
@@ -87,7 +87,7 @@
                     <div class="boxed">
                       <p> Fragentext</p>
                         <input id="ex1" data-slider-id='ex1Slider' type="text" data-slider-min="0" data-slider-max="20" data-slider-step="1" data-slider-value="14"/>
-                                            
+
                     </div>
                 </div>
             </div>
