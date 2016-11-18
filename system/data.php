c48f5d0df8d2e18df76b626fdfa3632fe860c32e
Maja Gobeli <maja.gobeli@mmp.htwchur.ch>
2016-11-18 09:30:22 +0100

    Registration auf index.php geschrieben, inkl. filter_data zusammen mit security.php und Funktionen in data.php zur Registration u. DB Verbindung

---
+++ system/data.php
@@ -0,0 +1,28 @@
+<?php
+
+//DB-Verbindung aufbauen
+function get_db_connection(){
+  $db = mysqli_connect('localhost','392356_4_1','RSNKf=MOVweQ','392356_4_1')
+    or die("Fehler beim Verbindungsaufbau mit dem Datenbank-Server.");
+  mysqli_set_charset($db,"utf-8");
+  return $db;
+}
+
+function get_result($sql){
+  echo $sql;
+  $db = get_db_connection();
+  $result = mysqli_query($db,$sql);
+  mysqli_close($db);
+  return $result;
+}
+
+/* *************************************************************************
+/* index.php
+/* ************************************************************************* */
+
+//Neuen User registrieren
+function register($firstname,$lastname,$taetigkeit,$email, $password){
+  $sql = "INSERT INTO user_tabelle (vorname,name,taetigkeit,email,passwort) VALUES ('$firstname','$lastname','$taetigkeit','$email','$password');";
+  return get_result($sql);
+}
+?>
